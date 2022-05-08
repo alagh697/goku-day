111249e67fb4390922f9b63fa25fed29ccc142c3
alagh697 <37829292+alagh697@users.noreply.github.com>
2022-05-08 15:46:32 +0200

    1 commit by Alaaeddine

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Footer() {
   return (
     <footer className="flex w-full items-center justify-center border-t border-gray-500/50 py-5">
-        <p>&copy; Ronindevo Copyright 2022.</p>
+        <p>&copy; <Link href="https://ronindevo.com/">
+          <a target="_blank" className="text-red-600 font-bold">Ronindevo</a></Link> Copyright 2022.</p>
     </footer>
   )
 }
